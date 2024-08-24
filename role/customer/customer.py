24c22c5dfa9b9f6c5d824de3ab8c555875cdab89
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-08-24 22:24:04 +0500

    Create add_customer

--- role/customer/customer.py
+++ role/customer/customer.py
@@ -28,3 +28,21 @@ class CostumerManager:
         with self.db as cursor:
             cursor.execute(query)
             return None
+
+
+    @log_decorator
+    def add_customer(self):
+        """
+        Add a new customer to the customers table.
+        """
+        threading.Thread(target=self.create_customer_table).start()
+        first_name = input("Customer first name: ").strip()
+        last_name = input("Customer last name: ").strip()
+        phone_number = input("Customer phone number: ").strip()
+        gmail = input("Customer gmail: ").strip()
+        query = '''INSERT INTO customers (first_name, last_name, phone_number, gmail) VALUES (%s, %s, %s, %s);'''
+        params = (first_name, last_name, phone_number, gmail)
+        threading.Thread(target=execute_query, args=(query, params)).start()
+        print(f"Customer '{first_name} {last_name}' added successfully.")
+        return None
+
