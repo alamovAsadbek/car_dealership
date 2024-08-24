9d6c80ec5d0157fc708c65d3c20f80e09379275c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-24 10:54:58 +0500

    Create auth_menu func in main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from main_files.decorator.decorator_func import log_decorator
+
+
+@log_decorator
+def auth_menu():
+    text = '''
+1. Login
+2. Logout
+    '''
+    print(text)
+    try:
+        user_input: int = int(input("Choose menu: "))
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            pass
+        else:
+            print("Invalid input")
+            auth_menu()
+    except Exception as e:
+        print(f'Error: {e}')
+        auth_menu()
