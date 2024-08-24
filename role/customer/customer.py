fc5c2b5c385c0a9fd6fc6c6759cee6b75afae570
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-08-24 22:22:12 +0500

    Create create_customer_table

--- role/customer/customer.py
+++ role/customer/customer.py
@@ -9,3 +9,22 @@ class CostumerManager:
     def __init__(self):
         self.db = Database()
 
+
+    @log_decorator
+    def create_customer_table(self):
+        """
+        Create the customers table in the database if it does not already exist.
+        """
+        query = '''
+            CREATE TABLE IF NOT EXISTS customers (
+            ID SERIAL PRIMARY KEY,
+            first_name VARCHAR(64) NOT NULL,
+            last_name VARCHAR(64) NOT NULL,
+            phone_number VARCHAR(64) NOT NULL,
+            gmail VARCHAR(64) NOT NULL,
+            CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            );
+        '''
+        with self.db as cursor:
+            cursor.execute(query)
+            return None
