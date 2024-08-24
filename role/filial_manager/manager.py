a04c729ba406013e01c61131c11cc06caef68559
Akhmedov76 <ahmadovj5016@gmail.com>
2024-08-24 21:14:41 +0500

    change add_car func

--- role/filial_manager/manager.py
+++ role/filial_manager/manager.py
@@ -1,15 +1,7 @@
 import threading
-import uuid
 
 from main_files.database.db_setting import Database, execute_query
 from main_files.decorator.decorator_func import log_decorator
-
-
-def generate_color_code():
-    """
-    Generate a unique UUID for color code.
-    """
-    return str(uuid.uuid4())
 
 
 class FilialManager:
@@ -24,11 +16,11 @@ class FilialManager:
         query = '''
                 CREATE TABLE IF NOT EXISTS car (
                 ID SERIAL PRIMARY KEY,
-                NAME VARCHAR(255) NOT NULL,  
-                BRAND_ID BIGINT NOT NULL REFERENCES brand(ID),
-                MODEL VARCHAR(255) NOT NULL,
+                NAME VARCHAR(255) NOT NULL,
                 YEAR INT NOT NULL,
+                MODEL_ID BIGINT NOT NULL REFERENCES MODEL(ID),
                 COLOR_ID BIGINT NOT NULL REFERENCES color(ID),
+                BRAND_ID BIGINT NOT NULL REFERENCES brand(ID),
                 FILIAL_ID BIGINT NOT NULL REFERENCES filial(ID),
                 CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
                 );
@@ -38,58 +30,24 @@ class FilialManager:
             return None
 
     @log_decorator
-    def create_car_color_table(self):
+    def add_car(self):
         """
-        Create the manager table in the database if it does not already exist.
+        Add a new car to the database.
         """
+        threading.Thread(target=self.create_car_table).start()
+        car_name = input("Enter car name: ")
+        brand_id = int(input("Enter car brand ID: "))
+        model = input("Enter car model: ")
+        year = int(input("Enter car year: "))
+        color_id = int(input("Enter car color ID: "))
+        filial_id = int(input("Enter car filial ID: "))
+
         query = '''
-                CREATE TABLE IF NOT EXISTS color (
-                ID SERIAL PRIMARY KEY,
-                NAME VARCHAR(255) NOT NULL,
-                CODE VARCHAR(255) NOT NULL
-                );
+                INSERT INTO car (NAME, BRAND_ID, MODEL, YEAR, COLOR_ID, FILIAL_ID) 
+                VALUES (%s, %s, %s, %s, %s, %s);
             '''
-        with self.db as cursor:
-            cursor.execute(query)
-            return None
-
-    def create_brand_table(self):
-        """
-        Create the car brand table in the database if it does not already exist.
-        """
-        query = '''
-                CREATE TABLE IF NOT EXISTS brand (
-                ID SERIAL PRIMARY KEY,
-                NAME VARCHAR(255) NOT NULL,
-                );
-            '''
-        with self.db as cursor:
-            cursor.execute(query)
-            return None
-
-    @log_decorator
-    def add_car_color(self):
-        """
-        Add a new car color to the database.
-        """
-        threading.Thread(target=self.create_car_color_table).start()
-        color_name = input("Enter car color name: ")
-        color_code = generate_color_code()
-
-        query = "INSERT INTO color (NAME, CODE) VALUES (%s, %s)"
-        threading.Thread(target=execute_query(query, (color_name, color_code)))
-        print(f"Car color added successfully with code {color_code}.")
+        threading.Thread(target=execute_query,
+                         args=(query, (car_name, brand_id, model, year, color_id, filial_id))).start()
+        print("Car added successfully.")
         return None
 
-    @log_decorator
-    def add_car_brand(self):
-        """
-        Add a new car brand to the database.
-        """
-        threading.Thread(target=self.create_brand_table).start()
-        brand_name = input("Enter car brand name: ")
-
-        query = "INSERT INTO brand (NAME) VALUES (%s)"
-        threading.Thread(target=execute_query(query, (brand_name,)))
-        print(f"Car brand added successfully.")
-        return None
