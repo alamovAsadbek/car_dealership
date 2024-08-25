76384d4859966cef580ad4cd69245178f50b3e77
Akhmedov76 <ahmadovj5016@gmail.com>
2024-08-25 17:33:38 +0500

    change user file

--- role/user/user.py
+++ role/user/user.py
@@ -2,12 +2,12 @@ import hashlib
 from main_files.database.db_setting import Database, execute_query
 from main_files.decorator.decorator_func import log_decorator
 
+
 class UserManager:
     def __init__(self):
         self.db = Database()
 
-
-def show_my_bought_cars(self):
+    def show_my_bought_cars(self):
         """
         Show my bought cars in the cars table.
         """
@@ -15,5 +15,6 @@ def show_my_bought_cars(self):
         result = execute_query(query, fetch="all")
         print("Boght Cars:")
         for car in result:
-            print(f"- ID: {car[0]}, Name: {car[1]}, Year: {car[2]}, Model_ID: {car[3]}, Color_ID: {car[4]}, Filial_ID: {car[5]}, Status: {car[6]}, Created_at : {car[7]}")
+            print(
+                f"- ID: {car[0]}, Name: {car[1]}, Year: {car[2]}, Model_ID: {car[3]}, Color_ID: {car[4]}, Filial_ID: {car[5]}, Status: {car[6]}, Created_at : {car[7]}")
         return None
